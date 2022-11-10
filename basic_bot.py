fd8293bea715e318a1a936fa0e426822f4743f96
Allen Shen <kuya5220@gmail.com>
2022-11-10 15:44:48 +0800

    Added a event that bot will leave vc after all user out.

--- basic_bot.py
+++ basic_bot.py
@@ -29,7 +29,17 @@ class Bot(commands.Bot):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
+    
+
 bot = Bot()
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    voice_state = member.guild.voice_client
+    if voice_state is not None and len(voice_state.channel.members) == 1:
+        # If the bot is connected to a channel and the bot is the only one in the channel
+        print("Bot getting out.")
+        await voice_state.disconnect() # Disconnect the bot from the channel
 
 async def load_extensions():
     for f in os.listdir("./cogs"):
