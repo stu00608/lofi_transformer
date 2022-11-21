9d92587237f1be67cddd59f8eba529332fdf4296
Allen Shen <kuya5220@gmail.com>
2022-11-21 11:32:01 +0800

    Added a time event to switch avatar.

--- basic_bot.py
+++ basic_bot.py
@@ -2,14 +2,16 @@
 
 import os
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from time import sleep
-import random
+from datetime import datetime
 import asyncio
-import json
 from generate import generate_song
 
 token = os.environ["BOT_TOKEN"]
+
+DAY_TIME = "06:00"
+NIGHT_TIME = "18:00"
 
 class Bot(commands.Bot):
     def __init__(self):
@@ -25,11 +27,21 @@ class Bot(commands.Bot):
             description=description
         )
 
+        now = datetime.now()
+        day_time = datetime.strptime(DAY_TIME, "%H:%M")
+        night_time = datetime.strptime(NIGHT_TIME, "%H:%M")
+        day_time = now.replace(hour=day_time.hour, minute=day_time.minute)
+        night_time = now.replace(hour=night_time.hour, minute=night_time.minute)
+        if day_time < now < night_time:
+            self.day_night_state = "day"
+            print("day now")
+        else:
+            self.day_night_state = "night"
+            print("night now")
+
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
-
-    
 
 bot = Bot()
 
@@ -41,6 +53,24 @@ async def on_voice_state_update(member, before, after):
         print("Bot getting out.")
         await voice_state.disconnect() # Disconnect the bot from the channel
 
+@tasks.loop(seconds=10)
+async def avator_update():
+    if bot.is_closed():
+        print("Bot is closed")
+        return
+
+    now = datetime.strftime(datetime.now(), '%H:%M')
+    if now == DAY_TIME and bot.day_night_state == "night":
+        with open('day_bocchi.jpg', 'rb') as image:
+            await bot.user.edit(avatar=image.read())
+            print("Good morning!")
+            bot.day_night_state = "day"
+    elif now == NIGHT_TIME and bot.day_night_state == "day":
+        with open('night_bocchi.jpg', 'rb') as image:
+            await bot.user.edit(avatar=image.read())
+            print("Good evening!")
+            bot.day_night_state = "night"
+    
 async def load_extensions():
     for f in os.listdir("./cogs"):
 	    if f.endswith(".py"):
@@ -48,6 +78,7 @@ async def load_extensions():
 
 async def main():
     async with bot:
+        avator_update.start()
         await load_extensions()
         await bot.start(token)
 
